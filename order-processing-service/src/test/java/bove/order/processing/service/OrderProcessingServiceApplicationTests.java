2c879d0c5dcf6971326421822b65e956ddf87e50
Emmanuel Tweneboah <emmanuel.tweneboah@turntabl.io>
2022-11-28 17:23:11 +0000

    project setup

---
+++ order-processing-service/src/test/java/bove/order/processing/service/OrderProcessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package bove.order.processing.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProcessingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
