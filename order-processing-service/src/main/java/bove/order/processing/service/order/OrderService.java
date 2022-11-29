396168156815b4a3cb923c4ed7cc0cd93e99d01a
Emmanuel Tweneboah <emmanuel.tweneboah@turntabl.io>
2022-11-29 17:08:33 +0000

    installed some dependencies to help implement the placing of order on the exchange service

--- order-processing-service/src/main/java/bove/order/processing/service/order/OrderService.java
+++ order-processing-service/src/main/java/bove/order/processing/service/order/OrderService.java
@@ -1,4 +1,32 @@
 package bove.order.processing.service.order;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
 public class OrderService {
+   @Value("${order.API_KEY}")
+   private String exchangeAPIkey;
+
+   @Value("${order.API_URL}")
+   private String exchangeURL;
+   public String placeOrder (Order order){
+      WebClient webClient = WebClient.create(exchangeURL);
+      try{
+         String response = webClient.post()
+                 .uri("/" + exchangeAPIkey + "/order")
+                 .body(Mono.just(order), Order.class)
+                 .retrieve()
+                 .bodyToMono(String.class)
+                 .block();
+         System.out.println(response);
+         return response;
+
+      }catch (Exception e){
+         // needs refactoring
+        return "Error"+e.getMessage();
+      }
+   };
 }
