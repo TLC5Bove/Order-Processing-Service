b65266d8d5fb97247ba7ea4f3731722553a32ba2
Emmanuel Tweneboah <emmanuel.tweneboah@turntabl.io>
2022-12-01 18:20:18 +0000

    implementation of getting order status and updating the db

---
+++ order-processing-service/src/main/java/bove/order/processing/service/controller/OrderController.java
@@ -0,0 +1,31 @@
+package bove.order.processing.service.controller;
+
+
+import bove.order.processing.service.dto.order.OrderRequest;
+import bove.order.processing.service.dto.order.OrderStatusResponse;
+import bove.order.processing.service.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/order")
+public class OrderController {
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public String placeOrderOnExchange(@RequestBody OrderRequest order) {
+        System.out.println(order);
+        return orderService.placeOrder(order);
+    }
+
+    @GetMapping("{orderId}")
+    public OrderStatusResponse getOrderStatus(OrderStatusResponse orderStatusResponse, @PathVariable String orderId) {
+        return orderService.getOrderStatus(orderId);
+    }
+
+}
--- order-processing-service/src/main/java/bove/order/processing/service/repository/OrderRepo.java
+++ order-processing-service/src/main/java/bove/order/processing/service/repository/OrderRepo.java
@@ -1,8 +1,8 @@
-package bove.order.processing.service.order;
+package bove.order.processing.service.repository;
 
+import bove.order.processing.service.dto.order.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Repository
 public interface OrderRepo extends JpaRepository<Order, String> {
