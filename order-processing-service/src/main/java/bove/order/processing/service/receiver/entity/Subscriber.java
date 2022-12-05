9e5e4b15db686b4bdd724e5fbe7df22897672d18
Emmanuel Tweneboah <emmanuel.tweneboah@turntabl.io>
2022-12-05 15:02:46 +0000

    imported Jackson2JsonRedisSerializer

--- order-processing-service/src/main/java/bove/order/processing/service/receiver/entity/Subscriber.java
+++ order-processing-service/src/main/java/bove/order/processing/service/receiver/entity/Subscriber.java
@@ -4,6 +4,7 @@ import bove.order.processing.service.config.MessagePublisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +13,7 @@ public class Subscriber implements MessageListener {
     @Autowired
     MessagePublisher messagePublisher;
     Jackson2JsonRedisSerializer serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+
     @Override
     public void onMessage(Message message, byte[] pattern) {
         Object obj = serializer.deserialize(message.getBody());
